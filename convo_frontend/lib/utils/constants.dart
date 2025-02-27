434a95acc0d655cfbd6d89a2a98557fd7e891b78
varxnnnn <varunkumar615768@gmail.com>
2025-02-27 23:16:07 +0530

    Second

--- convo_frontend/lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const String apiUrl = 'http://192.168.1.7:5000/api/auth'; // Backend API URL
-  static const String wsUrl = 'ws://192.168.1.7:5000/socket.io/'; // WebSocket URL
-}
